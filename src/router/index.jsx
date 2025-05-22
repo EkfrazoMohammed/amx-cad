7849a58baca5377aaaf25923e76c3f14cd107be6
MohammedIndusVisionAI <tayibulla@indusvision.ai>
2025-05-22 17:06:03 +0530

    update

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App.js";
-import UploadDxfView from "../views/UploadDxfView.jsx";
 import DxfViewerApp from "../views/DxfViewerApp.jsx";
 
 const router = createBrowserRouter([
@@ -8,7 +7,6 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      // { path: "/", element: <UploadDxfView /> },
       {path:'/',element:<DxfViewerApp />}
     ],
   },
